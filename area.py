d393e276e4b945add535b914db579f35a5844f5c
cnphuyen0601 <84063207+cnphuyen0601@users.noreply.github.com>
2021-05-14 20:53:15 +1000

    removed some unnecessary codes and added comment

--- area.py
+++ area.py
@@ -1,11 +1,10 @@
-#inputter to input the width of the regtangle
+# user to input the width of the rectangle
 width = float(input("Enter width: "))
-#inputter to input the height of the regtangle
+#  to input the height of the re tangle
 height = float(input("Enter height: "))
 area = width * height
 
-#print("Enter width: ", width)
-#print("Enter height: ", height)
+# print("Enter width: ", width)
+# print("Enter height: ", height)
 
 print("Area: ", area)
-
