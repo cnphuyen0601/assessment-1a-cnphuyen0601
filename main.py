869c9d4fa8c8091fb79cb051aad15b6cd48f47d1
cnphuyen0601 <84063207+cnphuyen0601@users.noreply.github.com>
2021-05-14 18:28:03 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+width = float(input("Enter the width : "))
+height = float(input("Enter the height : "))
+
+
+area = width * height
+
+
+
+print("Width",width)
+print("Height",height)
+print("Area", area)
+
+
